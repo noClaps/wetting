901bfae97e92f16f21db69a66c53d6ba2be1d4ba
JuicerOfApples <ethanjayhk@gmail.com>
2025-01-30 16:26:09 +0000

    Update problem7_starter_code.py

--- problem7_starter_code.py
+++ problem7_starter_code.py
@@ -11,13 +11,9 @@ matplotlib.rcParams['font.weight'] = 'bold'
 
 
 
-#Equation whose root defines the homogeneous equilibrium solutions:
 def func(beta,mu):
     return lambda rho : rho-(1.0-rho)*np.exp(beta*(mu+5.0*rho))
 
-
-
-# Helpful wrapper for the root finder in scipy which handles its annoying errors:
 def find_roots(f):
   roots = []
   #Compute a root over the entire range [0,1]
@@ -45,109 +41,100 @@ def find_roots(f):
       finish += step  #Enlarge interval until root can be found (if any)
   return roots
 
+def plotroots():
+    N = 100 #Density samples to use
+    rho = np.linspace(0,1,N) #Density values to evaluate
 
 
 
-#Main script:
-
-
-N = 100 #Density samples to use
-rho = np.linspace(0,1,N) #Density values to evaluate
+    #Evaluate the density profiles for the various parameter regimes
+    muVals = [-3.0]   #Chemical potentials to test
 
 
 
-#Evaluate the density profiles for the various parameter regimes
-muVals = [-3.0]   #Chemical potentials to test
+    # --- Low temperature case ---
+    beta = 2/3
+    funcs = [func(beta,mu) for mu in muVals] #Define a list of functions with the beta and mu specified
 
 
 
-# --- Low temperature case ---
-beta = 2/3
-funcs = [func(beta,mu) for mu in muVals] #Define a list of functions with the beta and mu specified
+    #Evaluate the functions for plotting
+    lowT = [f(rho) for f in funcs] #Returns a list of Nx1 float arrays
 
+    roots = [find_roots(f) for f in funcs] #Find the roots for each function
+    
+    fig1, ax1 = py.subplots()
+    for curve in lowT:
+        ax1.plot(rho,curve)
 
+    ax1.plot(roots, np.zeros(len(roots)), 'o', color = "red")
 
-#Evaluate the functions for plotting
-lowT = [f(rho) for f in funcs] #Returns a list of Nx1 float arrays
+    ax1.set_xlabel('Density Rho')
+    ax1.set_ylabel('Function')
 
-roots = [find_roots(f) for f in funcs] #Find the roots for each function
-
-
-
-
-# #Plot
-# fig1, ax1 = py.subplots()
-# for curve in lowT:
-#   ax1.plot(rho,curve)
-
-# ax1.plot(roots, np.zeros(len(roots)), 'o', color = "red")
-
-# ax1.set_xlabel('Density Rho')
-# ax1.set_ylabel('Function')
-
-kB = 1E-23
-a = 1E-23
-
-
-def rho_plus_minus(sign, yarr):
+def rho_plus_minus(sign, kBTe):
     # sign is +1 or -1
-    # return (1/2)*(1 + sign * np.sqrt(1 - 4*kB*yarr/(5*a)))
-    return (1/2)*(1 + sign * np.sqrt(1 - 4*kB*yarr/(5*a)))
+    return (1/2)*(1 + sign * np.sqrt(1 - 4*kBTe/5))
 
 def chem_pot_func():
     fig2, ax2 = py.subplots()
     ax2.set_ylim(0, 1.6)
 
-    yarr = np.linspace(0, 1.25, N)
+    kBTe = np.linspace(0, 1.25, N)
 
-    rho1 = rho_plus_minus(1, yarr)
-    rho2 = rho_plus_minus(-1, yarr)
-
-
-    xarr1 = kB*yarr*np.log(rho1/(rho2)) - 5*a*rho1
-    xarr2 = kB*yarr*np.log(rho2/(rho1)) - 5*a*rho2
-    xarr3 = np.ones(100)*(-5/2 * a)
+    rho1 = rho_plus_minus(1, kBTe)
+    rho2 = rho_plus_minus(-1, kBTe)
 
 
-    ax2.plot(xarr1,yarr, "blue")
-    ax2.plot(xarr2,yarr, "blue")
-    ax2.plot(xarr3,yarr, "red")
+    xarr1 = kBTe*np.log(rho1/(rho2)) - 5*rho1
+    xarr2 = kBTe*np.log(rho2/(rho1)) - 5*rho2
+    xarr3 = np.ones(100)*(-5/2)
+
+
+    ax2.plot(xarr1,kBTe, "blue")
+    ax2.plot(xarr2,kBTe, "blue")
+    ax2.plot(xarr3,kBTe, "red")
 
     ax2.plot(np.array([-2,-2.5,-3,-2,-2.5,-3])*1E-23,[1,1,1,3/2,3/2,3/2], 'o', color = "orange")
 
-betaarr = np.linspace(0,2,N)
+
+
+N = 100
+
+kBTarr = np.linspace(0.01,2,N)
 muarr = np.linspace(-5,0,N)
+densities = np.ones((N,N))
 
-funcs = []
-
-for beta in betaarr:
-    funcs += [func(beta, mu) for mu in muarr]
-
-roots = [find_roots(f) for f in funcs] #Find the roots for each function
-
-lowdensities = [min(i) if min(i) < 0.5 else 0 for i in roots]
-highdensities = [max(i) if max(i) > 0.5 else 0 for i in roots]
-
-def density_colormap():
-    fig2, ax2 = py.subplots()
-    ax2.set_ylim(0, 1.6)
-
-    yarr = np.linspace(0, 1.25, N)
-
-    rho1 = rho_plus_minus(1, yarr)
-    rho2 = rho_plus_minus(-1, yarr)
+for i in range(N):
+    for j in range(N):
+        
+        f = func(1/kBTarr[i], muarr[j])
+        roots = find_roots(f)
+        
+        if len(roots) == 1:
+            densities[i,j] = roots[0]
+        else:
+            densities[i,j] = -1
 
 
-    xarr1 = kB*yarr*np.log(rho1/(rho2)) - 5*a*rho1
-    xarr2 = kB*yarr*np.log(rho2/(rho1)) - 5*a*rho2
-    xarr3 = np.ones(100)*(-5/2 * a)
 
 
-    ax2.plot(xarr1,yarr, "blue")
-    ax2.plot(xarr2,yarr, "blue")
-    ax2.plot(xarr3,yarr, "red")
+kBTe = np.linspace(0, 1.25, N)
 
-    ax2.plot(xarr1, yarr, cmap=lowdensities)
-    py.show()
+rho1 = rho_plus_minus(1, kBTe)
+rho2 = rho_plus_minus(-1, kBTe)
 
-density_colormap()
+
+xarr1 = kBTe*np.log(rho1/(rho2)) - 5*rho1
+xarr2 = kBTe*np.log(rho2/(rho1)) - 5*rho2
+xarr3 = np.ones(100)*(-5/2)
+
+fig3, ax3 = py.subplots()
+
+py.pcolormesh(muarr, kBTarr, densities)
+
+py.plot(xarr1, kBTe, "blue")
+py.plot(xarr2, kBTe, "blue")
+py.plot(xarr3, kBTe, "red")
+
+
