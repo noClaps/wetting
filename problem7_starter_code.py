033411d6261874f42e59260173833330413837fd
noClaps <git@zerolimits.dev>
2025-01-30 15:14:10 +0000

    feat: Add some code

--- problem7_starter_code.py
+++ problem7_starter_code.py
@@ -22,35 +22,35 @@ def find_roots(f):
   roots = []
   #Compute a root over the entire range [0,1]
   sol = sp.optimize.brentq(f,0,1) #Use Brent's method - will find smallest root
-  roots.append(sol) 
-  
+  roots.append(sol)
+
   #Find if there are any other roots by using intervals above this solution
   tol = 1e-2 #Small shift above found root
   start = sol + tol
   step = 1e-1 #Small step used to find bounding interval for next root
   finish = sol + step
   #Continue looping and adding roots until finish becomes unphysical (>1)
-  
+
   while finish <= 1.0:
-    #Brent method will fail if f(start) and f(finish) have the same sign, 
+    #Brent method will fail if f(start) and f(finish) have the same sign,
     #so keep trying until it works:
-        
+
     try:
-      sol = sp.optimize.brentq(f,start,finish) 
-      roots.append(sol) 
+      sol = sp.optimize.brentq(f,start,finish)
+      roots.append(sol)
       start = sol + tol #Pick new interval start beyond root just found
       finish = sol + step #Assign new finish beyond it also and continue looping
-      
+
     except:
       finish += step  #Enlarge interval until root can be found (if any)
   return roots
-        
 
 
 
-#Main script:  
 
-    
+#Main script:
+
+
 N = 100 #Density samples to use
 rho = np.linspace(0,1,N) #Density values to evaluate
 
@@ -79,17 +79,18 @@ roots = [find_roots(f) for f in funcs] #Find the roots for each function
 # fig1, ax1 = py.subplots()
 # for curve in lowT:
 #   ax1.plot(rho,curve)
-  
+
 # ax1.plot(roots, np.zeros(len(roots)), 'o', color = "red")
 
 # ax1.set_xlabel('Density Rho')
 # ax1.set_ylabel('Function')
 
+kB = 1E-23
+a = 1E-23
 
 
-
-def rho_plus_minus(sign):
-    # sign is +1 or -1 
+def rho_plus_minus(sign, yarr):
+    # sign is +1 or -1
     # return (1/2)*(1 + sign * np.sqrt(1 - 4*kB*yarr/(5*a)))
     return (1/2)*(1 + sign * np.sqrt(1 - 4*kB*yarr/(5*a)))
 
@@ -97,14 +98,10 @@ def chem_pot_func():
     fig2, ax2 = py.subplots()
     ax2.set_ylim(0, 1.6)
 
-
-    kB = 1E-23
-    a = 1E-23
-
     yarr = np.linspace(0, 1.25, N)
 
-    rho1 = rho_plus_minus(1)
-    rho2 = rho_plus_minus(-1)
+    rho1 = rho_plus_minus(1, yarr)
+    rho2 = rho_plus_minus(-1, yarr)
 
 
     xarr1 = kB*yarr*np.log(rho1/(rho2)) - 5*a*rho1
@@ -126,37 +123,31 @@ funcs = []
 for beta in betaarr:
     funcs += [func(beta, mu) for mu in muarr]
 
-
 roots = [find_roots(f) for f in funcs] #Find the roots for each function
-print(roots)
 
-def low_mid_high_density(densities, lowhigh):
-    match lowhigh:
-        case "low":
-          lowdensities = []
-          for d in densities:
-             if d < 0.5:
-                lowdensities.append(d)
-             else:
-                lowdensities.append(0)
-          return lowdensities
-        case "high":
-          lowdensities = []
-          for d in densities:
-             if d < 0.5:
-                lowdensities.append(d)
-             else:
-                lowdensities.append(0)
-          return lowdensities
-          
-          
-        
-        
+lowdensities = [min(i) if min(i) < 0.5 else 0 for i in roots]
+highdensities = [max(i) if max(i) > 0.5 else 0 for i in roots]
 
-# lowdensities = np.fromiter((i for i in roots if ))
+def density_colormap():
+    fig2, ax2 = py.subplots()
+    ax2.set_ylim(0, 1.6)
 
-        
+    yarr = np.linspace(0, 1.25, N)
+
+    rho1 = rho_plus_minus(1, yarr)
+    rho2 = rho_plus_minus(-1, yarr)
 
 
+    xarr1 = kB*yarr*np.log(rho1/(rho2)) - 5*a*rho1
+    xarr2 = kB*yarr*np.log(rho2/(rho1)) - 5*a*rho2
+    xarr3 = np.ones(100)*(-5/2 * a)
 
 
+    ax2.plot(xarr1,yarr, "blue")
+    ax2.plot(xarr2,yarr, "blue")
+    ax2.plot(xarr3,yarr, "red")
+
+    ax2.plot(xarr1, yarr, cmap=lowdensities)
+    py.show()
+
+density_colormap()
